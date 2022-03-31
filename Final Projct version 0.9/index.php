fa5a02f0fee4da33e36e160375e02ad6717915f3
masha4321 <masharik09@mail.ru>
2022-03-30 23:05:26 -0400

    CSS Styling

--- Final Projct version 0.9/index.php
+++ Final Projct version 0.9/index.php
@@ -1,26 +1,30 @@
 <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Homepage</title>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Homepage</title>
     <link rel="stylesheet" href="css/index.css">
-    </head>
-    <body>
-        <div class="container">
-            <div class="maindiv">
-            <div class="success">Welcome to our banking system!</div>
+    <!--Roboto Font-->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <div class="container">
+        <div class="maindiv">
+            <div class="success">Welcome to SuperDuper Bank!</div>
             <div class="col-6">
-                <br>
-                <a href="log_in.php" class="href">Sign In</a> <br>
-                <a href="register.php" class="href">Register</a> <br>
-                <br>
-                <a href="admin_log_in.php" class="href">Administration</a> <br>
+                <button><a href="log_in.php" class="href">Sign In</a></button>
+                <button><a href="register.php" class="href">Register</a></button>
+                <button><a href="admin_log_in.php" class="href">Administration</a></button>
             </div>
             <div class="col-6"></div>
-            </div>
         </div>
-    </body>
-</html>
+    </div>
+</body>
 
+</html>
